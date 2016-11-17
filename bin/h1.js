1bcd7fdef73bd537b02a996a4b210295df6f1fff
Dawid Pregel <dawid.pregel@gmail.com>
2016-11-17 15:58:36 +0100

    WIP

--- bin/h1.js
+++ bin/h1.js
@@ -2,7 +2,6 @@
 
 const Cli = require('structured-cli');
 const Chalk = require('chalk');
-const Winston = require('Winston');
 
 const cli = Cli.createApp({
     epilog: Chalk.bold('>login<') + ' to obtain apiKey' + '\n'
--- bin/login.js
+++ bin/login.js
@@ -1,11 +1,13 @@
 const Cli = require('structured-cli');
-const Config = require('../../lib/config');
+const Config = require('../lib/config');
+const logger = new require('../lib/logger')();
+const util = require('util');
 
 module.exports = Cli.createCommand('login', {
 	description: 'Obtain your apiKey',
 	// plugins: [],
 	params: {
-		'user': {
+		'username': {
 			description: 'Your h1 username',
 			type: 'string'
 		}
@@ -14,5 +16,12 @@ module.exports = Cli.createCommand('login', {
 });
 
 function loginHandler(args) {
+	let configFile = new Config();
+
+	configFile.load().then(function(output) {
+		logger('debug', output);
+		logger('debug', util.inspect(args));
+	});
+
 
 }
